3a7c892aa072cd5067e1c6b061c534bebc2e39e4
prabhakar2k23 <pk912873@gmail.com>
2025-04-22 18:33:16 +0530

    String Concatenation

---
+++ js practice/js15.js
@@ -0,0 +1,10 @@
+let firstName = "Prabhakar";
+let lastName = "Kumar";
+let age = 28;
+let city = "Delhi";
+let occupation = "Software Engineer";
+
+let fullName = firstName + " " + lastName;
+let intro = "Hello, my name is " + fullName + ". I am " + age + " years old, working as a " + occupation + " and I live in " + city + ".";
+
+console.log(intro);
